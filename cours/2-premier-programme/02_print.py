aa0222c1765e87c21dea29b4e4d2e03432c302bc
quent <quentin.bidolet@gmail.com>
2023-02-06 12:31:57 +0100

    - print.py

---
+++ cours/2-premier-programme/02_print.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+# Paramètre sep pour changer le séparateur
+print('Hello', 'World', sep='-')
+
+# \ pour échapper un caractère
+print("J'ai besoin de citer \"\" ")
+
+# Retour chariot
+print("Ceci est la première ligne.\nCeci est la deuxième ligne.")
+
+# Dupliquer des strings
+print('#'*25)
+
+# String multiligne
+print("""Ceci est la première ligne.
+Ceci est la deuxième ligne.
+""")
