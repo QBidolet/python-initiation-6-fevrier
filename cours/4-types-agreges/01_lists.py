79ea1c45a7ae2e2ab7c91235deb19a019fd47bdf
quent <quentin.bidolet@gmail.com>
2023-02-07 10:00:11 +0100

    - lists.py

--- cours/4-types-agreges/01_lists.py
+++ cours/4-types-agreges/01_lists.py
@@ -31,7 +31,51 @@ print(liste.count(-99))
 print(len(liste))
 liste.append(-99)
 print(liste)
-liste.remove(50000)  # première occurrence uniquement
+# liste.remove(50000)  # première occurrence uniquement
 print(liste)
 
 del liste[0]
+
+resultat = liste.pop(0)
+print(resultat)
+
+liste = [1, 2, 3, 4]
+# attribution
+liste[0] = 12
+print(liste)
+
+# Copie
+garage = ["Clio", "Peugeot", "Ferrari"]
+garage_2 = garage
+print(id(garage), id(garage_2))
+garage[0] = "Volvo"
+print(garage_2)
+
+# Copy
+garage_1 = ["Ferrari", "Volvo", "Peugeot"]
+garage_2 = garage_1.copy()
+print(id(garage_1), id(garage_2))
+garage_1[0] = "Clio"
+print(garage_1, garage_2)
+
+# appartenance
+if "Clio" in garage_1:
+    print("Clio est dans le garage.")
+
+for voiture in garage_1:
+    print(voiture)
+
+# trier
+garage_2.sort(reverse=True)
+print(garage_2)
+
+# passer d'une liste à un string
+ma_liste = ["oui", "non", "ok"]
+print("/".join(ma_liste))
+
+# passer d'un string à une liste
+chaine = "oui:non:ok"
+liste = chaine.split(":")
+print(liste)
+
+
